93572bbdf9d370de908636bf411053f437799ce1
anelise.lopes <anelise.lopes@originalapp.com.br>
2020-02-12 00:04:52 -0300

    Sign Up e validação do form

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,34 +6,41 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { SignInComponent } from './home/signin/signin.component';
+import { SignUpComponent } from './home/signup/signup.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
-    { 
-        path: '', 
-        component: SignInComponent 
+    {
+        path: '',
+        component: SignInComponent,
+        canActivate: [AuthGuard]
     },
-    { 
-        path: 'user/:userName', 
+    {
+        path: 'signup',
+        component: SignUpComponent
+    },
+    {
+        path: 'user/:userName',
         component: PhotoListComponent,
         resolve: {
             photos: PhotoListResolver
         }
     },
-    { 
-        path: 'p/add', 
-        component: PhotoFormComponent 
+    {
+        path: 'p/add',
+        component: PhotoFormComponent
     },
-    { 
-        path: '**', 
-        component: NotFoundComponent 
-    }  
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
-    imports: [ 
-        RouterModule.forRoot(routes) 
+    imports: [
+        RouterModule.forRoot(routes)
     ],
-    exports: [ RouterModule ]
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
 
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'ap-header',
@@ -10,10 +11,13 @@ import { UserService } from '../user/user.service';
 export class HeaderComponent {
 
     user$: Observable<User>;
-    user: User;
 
-    constructor(userService: UserService) {
+    constructor(private userService: UserService, private router: Router) {
         this.user$ = userService.getUser();
-        this.user$.subscribe(user => this.user = user);
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
