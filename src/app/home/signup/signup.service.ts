afe2e2b14b8fc5acc8b278a8684ac261024c02b1
Anelise Lopes <anelopes17@outlook.com>
2020-02-18 21:44:28 -0300

    Criando validators

---
+++ src/app/home/signup/signup.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const URL_API = "http://localhost:3000";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SignUpService {
+
+    constructor(private http: HttpClient) { }
+
+    checkUserNameToken(userName: string) {
+        return this.http.get(URL_API + 'user/exists/' + userName);
+    }
+}
